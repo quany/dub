e2df6cf925e6c8868d1f2101d6c260cb73f1f772
Quany <quany@live.com>
2023-01-10 13:05:01 +0000

    fixbug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script'
 import "@/styles/globals.css";
 import cx from "classnames";
 import localFont from "@next/font/local";
