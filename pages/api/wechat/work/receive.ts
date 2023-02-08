b9afab667d0899a96788afecdef2919fd67d28e3
Quany <quany@live.com>
2023-02-08 17:53:11 +0800

    fixbug

--- pages/api/wechat/work/receive.ts
+++ pages/api/wechat/work/receive.ts
@@ -14,7 +14,7 @@ export default async function handler(
         console.log('req url:', req.url);
         console.log('req query:', req.query);
         const { echostr } = req.query as { echostr: string };
-        const msg = CryptoJS.AES.decrypt(echostr, '5xIKGoUYc3JwDWjhajNGBnUlomEJDJ45GV6eKobVcPu').toString(CryptoJS.enc.Utf8);
+        const msg = CryptoJS.AES.decrypt(CryptoJS.enc.Utf8.parse(echostr), '5xIKGoUYc3JwDWjhajNGBnUlomEJDJ45GV6eKobVcPu').toString(CryptoJS.enc.Utf8);
 
         console.log('decrypt content: ', msg);
 
