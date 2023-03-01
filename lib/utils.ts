9ab14f6d5bf98c58264635c96fb6331e7c0d18bd
Quany <quany@live.com>
2023-03-01 19:20:14 +0800

    spec: what

--- lib/utils.ts
+++ lib/utils.ts
@@ -179,7 +179,6 @@ export const getApexDomain = (url: string) => {
     // otherwise, it's a subdomain (e.g. dub.vercel.app), so we return the last 2 parts
     return parts.slice(-2).join(".");
   }
-  // if it's a normal domain (e.g. l0l.ink), we return the domain
   return domain;
 };
 
